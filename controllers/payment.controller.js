57309da6db59ac6f32ee33c151b571a607bd0078
baiyan412048 <39880236+baiyan412048@users.noreply.github.com>
2023-05-25 17:01:33 +0800

    add Line pay

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -1,142 +1,76 @@
+import fetch from 'node-fetch'
+import { config } from 'dotenv'
+
 // 失敗事件回應
 // import errorHandle from '../service/errorHandle.js'
 // 成功事件回應
 import successHandle from '../service/successHandle.js'
 
-import OPayPayment from 'opay_payment_nodejs/lib/opay_payment.js'
+import crypto from 'crypto'
+import { Buffer } from 'buffer'
 
-const randomValue = function (min, max) {
-  return Math.round(Math.random() * (max - min) + min)
-}
+config()
 
-export const payUid = (req, res) => {
-  const uid = randomValue(10, 99) + '1234567890234567' + randomValue(10, 99)
-  res.render('payment', { uid })
-}
+export const paymentRequest = async (req, res, next) => {
+  const channelSecret = process.env.Channel_SECRET_KEY
+  const requestUri = '/v3/payments/request'
+  const requestBody = JSON.stringify({
+    amount: 100, // 訂單總金額 = sum packages[].amount
+    currency: 'TWD',
+    orderId: Math.random().toString(36).substring(2, 5),
+    packages: [
+      {
+        id: '1',
+        amount: 100, // 商品總金額 = quantity * price
+        products: [
+          {
+            name: 'Pen Brown',
+            imageUrl: 'https://pay-store.line.com/images/pen_brown.jpg',
+            quantity: 2,
+            price: 50, // 結帳金額
+            originalPrice: 50 // 原始金額
+          }
+        ]
+      }
+    ],
+    redirectUrls: {
+      confirmUrl: 'http://127.0.0.1:3000/api/payment/',
+      cancelUrl: 'http://127.0.0.1:3000/api/payment/'
+    }
+  })
 
-// example: 2017/08/09 20:34:02
-const onTimeValue = function () {
-  const date = new Date()
-  const mm = date.getMonth() + 1
-  const dd = date.getDate()
-  const hh = date.getHours()
-  const mi = date.getMinutes()
-  const ss = date.getSeconds()
+  const nonce = Date.now().toString()
 
-  return [
-    date.getFullYear(),
-    '/' + (mm > 9 ? '' : '0') + mm,
-    '/' + (dd > 9 ? '' : '0') + dd,
-    ' ' + (hh > 9 ? '' : '0') + hh,
-    ':' + (mi > 9 ? '' : '0') + mi,
-    ':' + (ss > 9 ? '' : '0') + ss
-  ].join('')
-}
+  // 構建待簽名的字符串
+  const signString = channelSecret + requestUri + requestBody + nonce
 
-// 串連至歐付寶的金流服務（使用歐付寶的SDK）
-export const payAction = (req, res) => {
-  // const { uid } = req.body
+  // 使用 HMAC-SHA256 簽名計算
+  const signature = crypto
+    .createHmac('sha256', channelSecret)
+    .update(signString)
+    .digest()
 
-  const baseParam = {
-    MerchantTradeNo:
-      randomValue(10, 99) + '1234567890234567' + randomValue(10, 99), // 請帶20碼uid, ex: f0a0d7e9fae1bb72bc93
-    MerchantTradeDate: onTimeValue(), // ex: 2017/02/13 15:45:30
-    TotalAmount: '100',
-    TradeDesc: '企鵝玩偶 一隻',
-    ItemName: '企鵝玩偶 300元 X 1',
-    ReturnURL: 'https://api.baiyan777.com/api/payment/return', // 付款結果通知 URL
-    OrderResultURL: 'https://api.baiyan777.com/api/payment/result', // 在使用者在付款結束後，將使用者的瀏覽器畫面導向該 URL 所指定的 URL
-    EncryptType: 1,
-    // ItemURL: 'http://item.test.tw',
-    Remark: '該服務繳費成立時，恕不接受退款。'
-    // HoldTradeAMT: '1',
-    // StoreID: '',
-    // UseRedeem: ''
-  }
-
-  const create = new OPayPayment()
-  // let parameters = {}
-  // const invoice = {}
+  // Base64 編碼簽名
+  const base64Signature = Buffer.from(signature).toString('base64')
 
   try {
-    const htm = create.payment_client.aio_check_out_credit_onetime(baseParam)
+    const response = await fetch(
+      'https://sandbox-api-pay.line.me/v3/payments/request',
+      {
+        body: requestBody,
+        headers: {
+          'Content-Type': 'application/json',
+          'X-LINE-ChannelId': process.env.Channel_ID,
+          'X-LINE-Authorization-Nonce': Date.now().toString(),
+          'X-LINE-Authorization': base64Signature
+        },
+        method: 'POST'
+      }
+    )
 
-    successHandle(res, '測試 payment action', htm)
-  } catch (err) {
-    res.status(500).send(err)
+    const data = await response.json()
+    successHandle(res, '成功付款', data)
+  } catch (error) {
+    console.error(error)
   }
-}
-
-// 銜接歐付寶的 Return_URL 回來的資料，並確定付款是否成功，若成功則進行改變用戶資料動作。
-export const paymentResult = (req, res) => {
-  const rtnCode = req.body.RtnCode
-  const simulatePaid = req.body.SimulatePaid
-  const merchantID = req.body.MerchantID
-  const merchantTradeNo = req.body.MerchantTradeNo
-  const storeID = req.body.StoreID
-  const rtnMsg = req.body.RtnMsg
-  // var tradeNo = req.body.TradeNo;
-  const tradeAmt = req.body.TradeAmt
-  // var payAmt = req.body.PayAmt;
-  const paymentDate = req.body.PaymentDate
-  const paymentType = req.body.PaymentType
-  // var paymentTypeChargeFee = req.body.PaymentTypeChargeFee;
-
-  const paymentInfo = {
-    merchantID,
-    merchantTradeNo,
-    storeID,
-    rtnMsg,
-    paymentDate,
-    paymentType,
-    tradeAmt
-  }
-
-  // (添加simulatePaid模擬付款的判斷 1為模擬付款 0 為正式付款)
-  // 測試環境
-  if (rtnCode === '1' && simulatePaid === '1') {
-    // 這部分可與資料庫做互動
-
-    successHandle(res, '測試 Return URL', paymentInfo)
-    // res.write()
-    // res.end()
-  }
-  // 正式環境
-  //  else if (rtnCode === "1" && simulatePaid === "0") {
-  // 這部分可與資料庫做互動
-  // }
-  else {
-    res.status(400).send('0|err')
-    // res.write('0|err')
-    // res.end()
-  }
-}
-
-// 銜接歐付寶的 OrderResultURL
-export const paymentActionResult = (req, res) => {
-  const merchantID = req.body.MerchantID // 會員編號
-  const merchantTradeNo = req.body.MerchantTradeNo // 交易編號
-  const storeID = req.body.StoreID // 商店編號
-  const rtnMsg = req.body.RtnMsg // 交易訊息
-  const paymentDate = req.body.PaymentDate // 付款時間
-  const paymentType = req.body.PaymentType // 付款方式
-  const tradeAmt = req.body.TradeAmt // 交易金額
-
-  const result = {
-    member: {
-      merchantID,
-      merchantTradeNo,
-      storeID,
-      rtnMsg,
-      paymentDate,
-      paymentType,
-      tradeAmt
-    }
-  }
-  // console.log("result: " + JSON.stringify(result));
-
-  successHandle(res, '測試 Order Result URL', result)
-  // res.render('payment_result', {
-  //   result
-  // })
 }
--- routes/payment.router.js
+++ routes/payment.router.js
@@ -1,24 +1,27 @@
 import { Router } from 'express'
 
-import {
-  payUid,
-  payAction,
-  paymentResult,
-  paymentActionResult
-} from '../controllers/payment.controller.js'
+import { paymentRequest } from '../controllers/payment.controller.js'
 
 const router = Router()
 
+// 確認付款
+router.get('/confirm', function (req, res, next) {
+  res.send(`Line pay 確認付款 ${res}`)
+})
+
+// 付款
+router.get('/request', paymentRequest)
+
 // 用戶進入付款頁面所呼叫的 API
-router.get('/payment', payUid)
+// router.get('/payment', payUid)
 
 // 用戶在付款頁面按下結帳的 API
-router.get('/action', payAction)
+// router.get('/action', payAction)
 
 // 銜接歐付寶的 Return URL回來的資料
-router.post('/return', paymentResult)
+// router.post('/return', paymentResult)
 
 // 銜接歐付寶的 Order Result URL
-router.post('/result', paymentActionResult)
+// router.post('/result', paymentActionResult)
 
 export { router as payment }
